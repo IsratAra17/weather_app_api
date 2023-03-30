ecf0bf0610ab4c9768d62b4ca599cd9c60cb6b13
s.m.israt <smisratara25@gmail.com>
2023-03-30 22:02:57 +0600

    added Location Permission Code and take permission

---
+++ lib/weather_ui.dart
@@ -0,0 +1,76 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
+import 'package:jiffy/jiffy.dart';
+
+class Weather_UI extends StatefulWidget {
+  const Weather_UI({Key? key}) : super(key: key);
+
+  @override
+  State<Weather_UI> createState() => _Weather_UIState();
+}
+
+class _Weather_UIState extends State<Weather_UI> {
+
+  determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    position= await Geolocator.getCurrentPosition();
+    getWeatherData();
+    print("my latitude is ${position!.latitude}  longitute is ${position!.longitude}");
+  }
+
+
+  Position ?position;
+
+  //Map<String,dynamic>? weatherMap;
+ // Map<String,dynamic>? forecastMap;
+
+
+
+
+  getWeatherData(){}
+
+
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    determinePosition();
+    super.initState();
+  }
+
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
